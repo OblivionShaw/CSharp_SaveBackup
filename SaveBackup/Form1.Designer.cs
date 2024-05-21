adccbd566b3c48d8ae2052c8ff1a17c719384b39
OblivionShaw <sh960208@gmail.com>
2024-05-21 21:54:00 +0800

    feat:upload

---
+++ SaveBackup/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace SaveBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveLocalButton = new System.Windows.Forms.Button();
+            this.BackupLocalButton = new System.Windows.Forms.Button();
+            this.SaveLocalLabel = new System.Windows.Forms.Label();
+            this.BackupLocalLabel = new System.Windows.Forms.Label();
+            this.SaveBackupButton = new System.Windows.Forms.Button();
+            this.SaveBackupTips = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SaveLocalButton
+            // 
+            this.SaveLocalButton.Location = new System.Drawing.Point(142, 73);
+            this.SaveLocalButton.Name = "SaveLocalButton";
+            this.SaveLocalButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveLocalButton.TabIndex = 0;
+            this.SaveLocalButton.Text = "存檔位置";
+            this.SaveLocalButton.UseVisualStyleBackColor = true;
+            this.SaveLocalButton.Click += new System.EventHandler(this.SaveLocalButton_Click);
+            // 
+            // BackupLocalButton
+            // 
+            this.BackupLocalButton.Location = new System.Drawing.Point(142, 132);
+            this.BackupLocalButton.Name = "BackupLocalButton";
+            this.BackupLocalButton.Size = new System.Drawing.Size(75, 23);
+            this.BackupLocalButton.TabIndex = 1;
+            this.BackupLocalButton.Text = "備份位置";
+            this.BackupLocalButton.UseVisualStyleBackColor = true;
+            this.BackupLocalButton.Click += new System.EventHandler(this.BackupLocalButton_Click);
+            // 
+            // SaveLocalLabel
+            // 
+            this.SaveLocalLabel.AutoSize = true;
+            this.SaveLocalLabel.Location = new System.Drawing.Point(247, 73);
+            this.SaveLocalLabel.Name = "SaveLocalLabel";
+            this.SaveLocalLabel.Size = new System.Drawing.Size(33, 12);
+            this.SaveLocalLabel.TabIndex = 2;
+            this.SaveLocalLabel.Text = "label1";
+            // 
+            // BackupLocalLabel
+            // 
+            this.BackupLocalLabel.AutoSize = true;
+            this.BackupLocalLabel.Location = new System.Drawing.Point(247, 132);
+            this.BackupLocalLabel.Name = "BackupLocalLabel";
+            this.BackupLocalLabel.Size = new System.Drawing.Size(33, 12);
+            this.BackupLocalLabel.TabIndex = 3;
+            this.BackupLocalLabel.Text = "label2";
+            // 
+            // SaveBackupButton
+            // 
+            this.SaveBackupButton.Location = new System.Drawing.Point(286, 219);
+            this.SaveBackupButton.Name = "SaveBackupButton";
+            this.SaveBackupButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveBackupButton.TabIndex = 4;
+            this.SaveBackupButton.Text = "執行備份";
+            this.SaveBackupButton.UseVisualStyleBackColor = true;
+            this.SaveBackupButton.Click += new System.EventHandler(this.SaveBackupButton_Click);
+            // 
+            // SaveBackupTips
+            // 
+            this.SaveBackupTips.AutoSize = true;
+            this.SaveBackupTips.Location = new System.Drawing.Point(455, 219);
+            this.SaveBackupTips.Name = "SaveBackupTips";
+            this.SaveBackupTips.Size = new System.Drawing.Size(33, 12);
+            this.SaveBackupTips.TabIndex = 5;
+            this.SaveBackupTips.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveBackupTips);
+            this.Controls.Add(this.SaveBackupButton);
+            this.Controls.Add(this.BackupLocalLabel);
+            this.Controls.Add(this.SaveLocalLabel);
+            this.Controls.Add(this.BackupLocalButton);
+            this.Controls.Add(this.SaveLocalButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveLocalButton;
+        private System.Windows.Forms.Button BackupLocalButton;
+        private System.Windows.Forms.Label SaveLocalLabel;
+        private System.Windows.Forms.Label BackupLocalLabel;
+        private System.Windows.Forms.Button SaveBackupButton;
+        private System.Windows.Forms.Label SaveBackupTips;
+    }
+}
+
---
+++ SaveBackup/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace SaveBackup
+{
+    public partial class Form1 : Form
+    {
+        private string sourcePath;
+        private string backupPath;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sourcePath = Properties.Settings.Default.SourcePath;
+            backupPath = Properties.Settings.Default.BackupPath;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+
+        private void SaveLocalButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    sourcePath = fbd.SelectedPath;
+                    SaveLocalLabel.Text = sourcePath;
+
+                    // 保存設定
+                    Properties.Settings.Default.SourcePath = sourcePath;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void BackupLocalButton_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    backupPath = fbd.SelectedPath;
+                    BackupLocalLabel.Text = backupPath;
+
+                    // 保存設定
+                    Properties.Settings.Default.BackupPath = backupPath;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void SaveBackupButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (var file in Directory.GetFiles(sourcePath))
+                {
+                    File.Copy(file, Path.Combine(backupPath, Path.GetFileName(file)), true);
+                }
+                SaveBackupTips.Text = "備份成功";
+            }
+            catch (Exception ex)
+            {
+                SaveBackupTips.Text = "備份失敗: " + ex.Message;
+            }
+
+        }
+    }
+}
